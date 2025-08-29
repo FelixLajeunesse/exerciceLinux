40e417eeb9056e9677e534d33efe125b1aede02f
FelixLajeunesse <6310376@cstjean.qc.ca>
2025-08-29 17:10:29 -0400

    [commit]

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Creation des dossiers bin, cegep et projets"
+mkdir bin cegep projets
+
+echo "Installation de git" 
+sudo pacman -S git
+
+echo "Installation de vim"
+sudo pacman -S vim
+
+echo "Installation de gcc"
+sudo pacman -S gcc
+
+echo "Installation de gdb"
+sudo pacman -S gdb
+
+echo "Installation de python3"
+sudo pacman -S python3
+
+echo "Installation de python-pip"
+sudo pacman -S python-pip
+
+
